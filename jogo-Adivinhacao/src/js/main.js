e630771d138ef91bb757497f147406b576ee7f52
WagnerRodrigues <felten19@gmail.com>
2022-12-27 22:25:25 -0300

    feito modificacoes no js e colocado evento no botao de jogar novamente

--- jogo-Adivinhacao/src/js/main.js
+++ jogo-Adivinhacao/src/js/main.js
@@ -1,27 +1,43 @@
+let randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
 
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+let card1 = document.querySelector(".card1");
+let card2 = document.querySelector(".card2");
 
-function handleClick(event){
-    event.preventDefault()
+let numberInput = document.querySelector("#number");
 
-    let inputNumber = document.querySelector('#number')
+function handleClick(event) {
+  event.preventDefault();
 
-    if(Number(inputNumber.value) == randomNumber){
-        document.querySelector('.card1').classList.add("disappear")
-        document.querySelector('.card2').classList.remove("disappear")
+  let inputNumber = numberInput;
 
-        document.querySelector(".card2 h1").innerText = `Você acertou em ${xAttempts} tentativas. o numero sortiado foi ${randomNumber}`
+  if (Number(inputNumber.value) == randomNumber) {
+    card1.classList.add("disappear");
+    card2.classList.remove("disappear");
 
-       
-    }else{
-        alert("Você errou!!")
-    }
+    document.querySelector(
+      ".card2 h1"
+    ).innerText = `Você acertou em ${xAttempts} tentativas. o numero sortiado foi ${randomNumber}`;
+  }
 
-    xAttempts++
+  if (Number(numberInput.value) < 0 || Number(numberInput.value) > 10) {
+    alert("O número tem que ser entre 1 e 10");
+  } else {
+    console.log("numero valido");
+  }
+
+  if(Number(inputNumber.value) == ''){
+    xAttempts = 0
+    alert("Tem que colocar um valor!!")
+  }
+  xAttempts++;
 }
 
-const reset = (event) => {
-    
-}
+function btnReset() {
+  card1.classList.remove("disappear");
+  card2.classList.add("disappear");
 
+  numberInput.value = " ";
+  randomNumber = Math.round(Math.random() * 10);
+  xAttempts = 1;
+}
